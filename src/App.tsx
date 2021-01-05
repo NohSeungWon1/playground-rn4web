ed1c40c03f7f2c227652144925722b47bd48b64b
NohSeungWon1 <sw3noh@cartool.co>
2021-01-05 10:35:08 +0900

    webVitals init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import First from './component/First';
 import store from './reduxs/store';
+import webVitals from './condition-check/webVitals';
+webVitals(console.log);
 
 const App = () => {
   return (
